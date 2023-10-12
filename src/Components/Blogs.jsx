2fda8c4c3466f1dd14c880fc330763c3e5b53af2
arunavajana99 <arunavajana47@gmail.com>
2023-10-12 23:41:41 +0530

    design changes done

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -5,7 +5,7 @@ import Spinner from "./Spinner";
 const Blogs = () => {
   const { loading, posts } = useContext(AppContext);
   return (
-    <div className="max-w-[670px] w-11/12 py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center h-screen">
+    <div className="max-w-[670px] w-11/12 py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center">
       {loading ? (
         <Spinner />
       ) : posts.length === 0 ? (
@@ -13,7 +13,7 @@ const Blogs = () => {
       ) : (
         posts.map((post) => (
           <div key={post.id}>
-            <p className="font-bold text-sm">{post.title}</p>
+            <p className="font-bold text-lg">{post.title}</p>
             <p className="text-[14px]">
               By <span className="italic">{post.author}</span> on{" "}
               <span className="underline font-bold">{post.category}</span>
@@ -24,7 +24,7 @@ const Blogs = () => {
               {post.tags.map((tag, index) => (
                 <span
                   key={index}
-                  className="text-xs font-semibold underline text-blue-700 cursor-pointer">{`#${tag}`}</span>
+                  className="text-xs font-semibold underline text-blue-700 cursor-pointer mt-[5px]">{`#${tag}`}</span>
               ))}
             </div>
           </div>
